1a6b5c4454c05e7bfc0cae38a9f59dc44d7500ad
Isa <isakhaann@gmail.com>
2023-09-20 15:34:47 +0300

    Added method that returns max and min

--- assignment2.java
+++ assignment2.java
@@ -2,6 +2,23 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class assignment2 {
+    public static int[] max_min(int arr[])
+    {
+        int min=99;
+        int max=-99;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>max)
+            max=arr[i];
+            if(arr[i]<min)
+            min=arr[i];
+        }
+        int res[] = new int[2];
+        res[0]=max;
+        res[1]=min;
+        
+        return res;
+    }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Random rn = new Random(0);
@@ -14,7 +31,12 @@ public class assignment2 {
             int rand = rn.nextInt(0,100);
             arr[i] = rand;
         }
-        
+        System.out.println();
+        for(int i = 0; i<size; i++){
+            System.out.println(arr[i]);
+            
+        }
+        System.out.println("max is "+ max_min(arr)[0]+" min is "+max_min(arr)[1]);
         
 
     }
